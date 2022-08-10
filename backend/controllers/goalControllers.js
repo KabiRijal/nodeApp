664d9e1050bd514ef3bb8464131e9eabb6d09d03
Kabindra Rijal <kabindra.rijal@sevadev.com>
2022-08-10 16:48:07 +0545

    Add CRUD functionality of goal

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,35 +1,64 @@
 const asyncHandler = require("express-async-handler")
 
+const Goal = require("../models/goalModel")
+
 //@desc     Get goals
 //routes    GET /api/goals
 //@access   Private
-const getGoal = asyncHandler((req, res) => {
-  res.status(200).json({ message: "Get goals" })
+const getGoal = asyncHandler(async (req, res) => {
+  const goals = await Goal.find()
+
+  res.status(200).json(goals)
 })
 
 //@desc     set goals
 //routes    POST /api/goals/:id
 //@access   Private
-const setGoal = asyncHandler((req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400)
     throw new Error("Please enter text")
   }
-  res.status(200).json({ message: "Set Goals" })
+
+  const goal = await Goal.create({
+    text: req.body.text,
+  })
+
+  res.status(200).json(goal)
 })
 
 //@desc     update goals
 //routes    PUT /api/goals
 //@access   Private
-const updateGoal = asyncHandler((req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` })
+const updateGoal = asyncHandler(async (req, res) => {
+  const goal = Goal.findById(req.params.id)
+
+  if (!goal) {
+    res.status(400)
+    throw new Error("Goal not found")
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  })
+
+  res.status(200).json(updatedGoal)
 })
 
 //@desc     Delete goals
 //routes    DELETE /api/goals/:id
 //@access   Private
-const deleteGoal = asyncHandler((req, res) => {
-  res.status(200).json({ message: `delete goal ${req.params.id}` })
+const deleteGoal = asyncHandler(async (req, res) => {
+
+  const goal = await Goal.findById(req.params.id)
+
+  if(!goal){
+    res.status(400)
+    throw new Error('goal not found')
+  }
+
+  await goal.remove
+  res.status(200).json({id:req.params.id})
 })
 
 module.exports = {
