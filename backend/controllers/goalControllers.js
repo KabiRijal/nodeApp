7f1142811fd6e652de889332ea82f7cd767220d6
Kabindra Rijal <kabindra.rijal@sevadev.com>
2022-08-17 21:52:28 +0545

    CRUD with authorization and authentication

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,12 +1,13 @@
 const asyncHandler = require("express-async-handler")
 
 const Goal = require("../models/goalModel")
+const User = require('../models/userModel')
 
 //@desc     Get goals
 //routes    GET /api/goals
 //@access   Private
 const getGoal = asyncHandler(async (req, res) => {
-  const goals = await Goal.find()
+  const goals = await Goal.find({ user: req.user.id })
 
   res.status(200).json(goals)
 })
@@ -22,6 +23,7 @@ const setGoal = asyncHandler(async (req, res) => {
 
   const goal = await Goal.create({
     text: req.body.text,
+    user: req.user.id,
   })
 
   res.status(200).json(goal)
@@ -38,6 +40,20 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found")
   }
 
+  const user = await User.findById(req.user.id)
+
+  //Check for user
+  if(!user){
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  //Make sure the logged in user matches the goal user
+   if(goal.user.toString() !== user.id){
+    res.status(201)
+    throw new Error('User not authorized')
+   }
+
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   })
@@ -49,16 +65,29 @@ const updateGoal = asyncHandler(async (req, res) => {
 //routes    DELETE /api/goals/:id
 //@access   Private
 const deleteGoal = asyncHandler(async (req, res) => {
-
   const goal = await Goal.findById(req.params.id)
 
-  if(!goal){
+  if (!goal) {
     res.status(400)
-    throw new Error('goal not found')
+    throw new Error("goal not found")
   }
 
-  await goal.remove
-  res.status(200).json({id:req.params.id})
+  const user = await User.findById(req.user.id)
+
+  //Check for user
+  if(!user){
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  //Make sure the logged in user matches the goal user
+   if(goal.user.toString() !== user.id){
+    res.status(201)
+    throw new Error('User not authorized')
+   }
+
+  await goal.remove()
+  res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,6 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     })
   } else {
     res.status(400)
@@ -59,6 +60,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     })
   } else {
     res.status(400)
@@ -66,12 +68,25 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 })
 
-//@desc     Get User deta
+//@desc     Get User data
 //routes    GET /api/Users/me
-//@access   Public
+//@access   Private
 const getMe = asyncHandler(async (req, res) => {
-  res.json({ message: "User data" })
+  const { _id, name, email } = await User.findById(req.user.id)
+
+  res.status(200).json({
+    id: _id,
+    name,
+    email,
+  })
 })
+
+//Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  })
+}
 
 module.exports = {
   registerUser,
